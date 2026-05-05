87d120b5aa45dfce47393619c6241bd8c9256975
alin anto <alinanto5@gmail.com>
2026-05-05 06:27:11 +0000

    added syncDB.sh

---
+++ syncDB.sh
@@ -0,0 +1,7 @@
+rsync -rv --exclude 'venv/' \
+          --exclude 'static/admin/' \
+          --exclude '__pycache__/' \
+          --exclude '*.log' \
+          --exclude '.env' \
+          --exclude '.git/' \
+        CloudGenie:/var/www/portfolio/db.sqlite3 ./
